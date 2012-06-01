82bc803423c3d3a63d398fc9ee92bfd5c2c30170
Daniel Berger <djberg96@gmail.com>
2012-05-31 18:24:48 -0600

    Added the Process.get_affinity method and related functions.

--- lib/win32/process.rb
+++ lib/win32/process.rb
@@ -5,6 +5,33 @@ module Process
   include Process::Constants
   extend Process::Functions
 
+  def get_affinity(int = Process.pid)
+    pmask = FFI::MemoryPointer.new(:ulong)
+    smask = FFI::MemoryPointer.new(:ulong)
+
+    if int == Process.pid
+      unless GetProcessAffinityMask(GetCurrentProcess(), pmask, smask)
+        raise SystemCallError, FFI.errno, "GetProcessAffinityMask"
+      end
+    else
+      begin
+        handle = OpenProcess(PROCESS_QUERY_INFORMATION, 0 , int)
+
+        if handle == INVALID_HANDLE_VALUE
+          raise SystemCallError, FFI.errno, "OpeProcess"
+        end
+
+        unless GetProcessAffinityMask(handle, pmask, smask)
+          raise SystemCallError, FFI.errno, "GetProcessAffinityMask"
+        end
+      ensure
+        CloseHandle(handle)
+      end
+    end
+
+    [pmask.read_long, smask.read_long]
+  end
+
   def getpriority(kind, int)
     raise TypeError, kind unless kind.is_a?(Fixnum) # Match spec
     raise TypeError, int unless int.is_a?(Fixnum)   # Match spec
@@ -30,4 +57,5 @@ module Process
   end
 
   module_function :getpriority
+  module_function :get_affinity
 end
--- lib/win32/process/functions.rb
+++ lib/win32/process/functions.rb
@@ -6,6 +6,8 @@ module Process::Functions
   ffi_lib :kernel32
 
   attach_function :CloseHandle, [:ulong], :bool
+  attach_function :GetCurrentProcess, [], :ulong
+  attach_function :GetProcessAffinityMask, [:ulong, :pointer, :pointer], :bool
   attach_function :GetPriorityClass, [:ulong], :ulong
   attach_function :OpenProcess, [:ulong, :bool, :ulong], :ulong
 end
