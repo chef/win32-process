99f4d5f7f16a3d9007eca3b8e6bc4b215190a4fe
Daniel Berger <djberg96@gmail.com>
2012-06-06 21:54:48 -0600

    Initial implementation of Process.create.

--- lib/win32/process.rb
+++ lib/win32/process.rb
@@ -267,6 +267,220 @@ module Process
         at_exit{ CloseHandle(handle) if handle }
       end
     end
+
+    def create(args)
+      unless args.kind_of?(Hash)
+        raise TypeError, 'hash keyword arguments expected'
+      end
+
+      valid_keys = %w[
+        app_name command_line inherit creation_flags cwd environment
+        startup_info thread_inherit process_inherit close_handles with_logon
+        domain password
+      ]
+
+      valid_si_keys = %w[
+        startf_flags desktop title x y x_size y_size x_count_chars
+        y_count_chars fill_attribute sw_flags stdin stdout stderr
+      ]
+
+      # Set default values
+      hash = {
+        'app_name'       => nil,
+        'creation_flags' => 0,
+        'close_handles'  => true
+      }
+
+      # Validate the keys, and convert symbols and case to lowercase strings.
+      args.each{ |key, val|
+        key = key.to_s.downcase
+        unless valid_keys.include?(key)
+          raise ArgumentError, "invalid key '#{key}'"
+        end
+        hash[key] = val
+      }
+
+      si_hash = {}
+
+      # If the startup_info key is present, validate its subkeys
+      if hash['startup_info']
+        hash['startup_info'].each{ |key, val|
+          key = key.to_s.downcase
+          unless valid_si_keys.include?(key)
+            raise ArgumentError, "invalid startup_info key '#{key}'"
+          end
+          si_hash[key] = val
+        }
+      end
+
+      # The +command_line+ key is mandatory unless the +app_name+ key
+      # is specified.
+      unless hash['command_line']
+        if hash['app_name']
+          hash['command_line'] = hash['app_name']
+          hash['app_name'] = nil
+        else
+          raise ArgumentError, 'command_line or app_name must be specified'
+        end
+      end
+
+      env = nil
+
+      # The env string should be passed as a string of ';' separated paths.
+      if hash['environment']
+        env = hash['environment'].split(File::PATH_SEPARATOR) << 0.chr
+
+        if hash['with_logon']
+          env = env.map{ |e| e.encode('UTF-16LE') }
+        end
+
+        env = env.join("\0")
+      end
+
+      # Process SECURITY_ATTRIBUTE structure
+      process_security = nil
+
+      if hash['process_inherit']
+        process_security = SECURITY_ATTRIBUTES.new
+        process_security[:nLength] = 12
+        process_security[:bInheritHandle] = true
+      end
+
+      # Thread SECURITY_ATTRIBUTE structure
+      thread_security = nil
+
+      if hash['thread_inherit']
+        thread_security = SECURITY_ATTRIBUTES.new
+        thread_security[:nLength] = 12
+        thread_security[:bInheritHandle] = true
+      end
+
+      # Automatically handle stdin, stdout and stderr as either IO objects
+      # or file descriptors. This won't work for StringIO, however.
+      ['stdin', 'stdout', 'stderr'].each{ |io|
+        if si_hash[io]
+          if si_hash[io].respond_to?(:fileno)
+            handle = get_osfhandle(si_hash[io].fileno)
+          else
+            handle = get_osfhandle(si_hash[io])
+          end
+
+          if handle == INVALID_HANDLE_VALUE
+            raise SystemCallError, FFI.errno, "get_osfhandle"
+          end
+
+          # Most implementations of Ruby on Windows create inheritable
+          # handles by default, but some do not. RF bug #26988.
+          bool = SetHandleInformation(
+            handle,
+            HANDLE_FLAG_INHERIT,
+            HANDLE_FLAG_INHERIT
+          )
+
+          raise SystemCallError, FFI.errno, "SetHandleInformation" unless bool
+
+          si_hash[io] = handle
+          si_hash['startf_flags'] ||= 0
+          si_hash['startf_flags'] |= STARTF_USESTDHANDLES
+          hash['inherit'] = true
+        end
+      }
+
+      procinfo  = PROCESS_INFORMATION.new
+      startinfo = STARTUPINFO.new
+
+      unless si_hash.empty?
+        startinfo[:cb]              = startinfo.size
+        startinfo[:lpReserved]      = nil
+        startinfo[:lpDesktop]       = si_hash['desktop']
+        startinfo[:lpTitle]         = si_hash['title']
+        startinfo[:dwX]             = si_hash['x']
+        startinfo[:dwY]             = si_hash['y']
+        startinfo[:dwXSize]         = si_hash['x_size']
+        startinfo[:dwYSize]         = si_hash['y_size']
+        startinfo[:dwXCountChars]   = si_hash['x_count_chars']
+        startinfo[:dwYCountChars]   = si_hash['y_count_chars']
+        startinfo[:dwFillAttribute] = si_hash['fill_attribute']
+        startinfo[:dwFlags]         = si_hash['startf_flags']
+        startinfo[:wShowWindow]     = si_hash['sw_flags']
+        startinfo[:cbReserved2]     = 0
+        startinfo[:lpReserved2]     = nil
+        startinfo[:hStdInput]       = si_hash['stdin']
+        startinfo[:hStdOutput]      = si_hash['stdout']
+        startinfo[:hStdError]       = si_hash['stderr']
+      end
+
+      if hash['with_logon']
+        app = nil
+        cmd = nil
+
+        logon  = (hash['with_logon'] + "\0").encode('UTF-16LE')
+        domain = (hash['domain'] + "\0").encode('UTF-16LE')
+        cwd    = (hash['cwd'] + "\0").encode('UTF-16LE')
+        passwd = (hash['password'] + "\0").encode('UTF-16LE')
+
+        if hash['app_name']
+          app = (hash['app_name'] + "\0").encode('UTF-16LE')
+        end
+
+        if hash['command_line']
+          cmd = (hash['command_line'] + "\0").encode('UTF-16LE')
+        end
+
+        hash['creation_flags'] |= CREATE_UNICODE_ENVIRONMENT
+
+        bool = CreateProcessWithLogonW(
+          logon,                  # User
+          domain,                 # Domain
+          passwd,                 # Password
+          LOGON_WITH_PROFILE,     # Logon flags
+          app,                    # App name
+          cmd,                    # Command line
+          hash['creation_flags'], # Creation flags
+          env,                    # Environment
+          cwd,                    # Working directory
+          startinfo,              # Startup Info
+          procinfo                # Process Info
+        )
+
+        unless bool
+          raise SystemCallError, FFI.errno, "CreateProcessWithLogonW"
+        end
+      else
+        inherit = hash['inherit'] || false
+
+        bool = CreateProcessA(
+          hash['app_name'],       # App name
+          hash['command_line'],   # Command line
+          process_security,       # Process attributes
+          thread_security,        # Thread attributes
+          inherit,                # Inherit handles?
+          hash['creation_flags'], # Creation flags
+          env,                    # Environment
+          hash['cwd'],            # Working directory
+          startinfo,              # Startup Info
+          procinfo                # Process Info
+        )
+
+        unless bool
+          raise SystemCallError, FFI.errno, "CreateProcess"
+        end
+      end
+
+      # Automatically close the process and thread handles in the
+      # PROCESS_INFORMATION struct unless explicitly told not to.
+      if hash['close_handles']
+        CloseHandle(procinfo[:hProcess])
+        CloseHandle(procinfo[:hThread])
+      end
+
+      ProcessInfo.new(
+        procinfo[:hProcess],
+        procinfo[:hThread],
+        procinfo[:dwProcessId],
+        procinfo[:dwThreadId]
+      )
+    end
   end
 
   private
