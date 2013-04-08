a4be957d960d05edeebfdea89fa5979e2eb36273
Daniel Berger <djberg96@gmail.com>
2013-04-07 22:52:18 -0600

    Added custom data types in Process::Functions and fixed the HANDLE data types.

--- lib/win32/process/functions.rb
+++ lib/win32/process/functions.rb
@@ -16,53 +16,58 @@ module Process::Functions
 
   extend FFI::Library
 
+  typedef :ulong, :dword
+  typedef :uintptr_t, :handle
+  typedef :uintptr_t, :hwnd
+  typedef :uintptr_t, :hmodule
+
   ffi_lib :kernel32
 
-  attach_pfunc :CloseHandle, [:ulong], :bool
-  attach_pfunc :GenerateConsoleCtrlEvent, [:ulong, :ulong], :bool
-  attach_pfunc :GetCurrentProcess, [], :ulong
-  attach_pfunc :GetModuleHandle, :GetModuleHandleA, [:string], :ulong
-  attach_pfunc :GetProcessAffinityMask, [:ulong, :pointer, :pointer], :bool
-  attach_pfunc :GetPriorityClass, [:ulong], :ulong
-  attach_pfunc :GetProcAddress, [:ulong, :string], :ulong
+  attach_pfunc :CloseHandle, [:handle], :bool
+  attach_pfunc :GenerateConsoleCtrlEvent, [:dword, :dword], :bool
+  attach_pfunc :GetCurrentProcess, [], :handle
+  attach_pfunc :GetModuleHandle, :GetModuleHandleA, [:string], :hmodule
+  attach_pfunc :GetProcessAffinityMask, [:handle, :pointer, :pointer], :bool
+  attach_pfunc :GetPriorityClass, [:handle], :dword
+  attach_pfunc :GetProcAddress, [:hmodule, :string], :ulong
   attach_pfunc :GetVersionExA, [:pointer], :bool
-  attach_pfunc :IsProcessInJob, [:ulong, :pointer, :pointer], :void
-  attach_pfunc :OpenProcess, [:ulong, :bool, :ulong], :ulong
-  attach_pfunc :SetHandleInformation, [:ulong, :ulong, :ulong], :bool
+  attach_pfunc :IsProcessInJob, [:handle, :pointer, :pointer], :bool # 2nd arg optional
+  attach_pfunc :OpenProcess, [:dword, :bool, :dword], :handle
+  attach_pfunc :SetHandleInformation, [:handle, :dword, :dword], :bool
   attach_pfunc :SetErrorMode, [:uint], :uint
-  attach_pfunc :SetPriorityClass, [:ulong, :ulong], :bool
-  attach_pfunc :TerminateProcess, [:ulong, :uint], :bool
-  attach_pfunc :WaitForSingleObject, [:ulong, :ulong], :ulong
+  attach_pfunc :SetPriorityClass, [:handle, :dword], :bool
+  attach_pfunc :TerminateProcess, [:handle, :uint], :bool
+  attach_pfunc :WaitForSingleObject, [:handle, :dword], :dword
 
   attach_pfunc :CreateRemoteThread,
-    [:ulong, :pointer, :size_t, :ulong, :pointer, :ulong, :pointer], :ulong
+    [:handle, :pointer, :size_t, :ulong, :pointer, :dword, :pointer], :handle
 
   attach_pfunc :GetVolumeInformationA,
-    [:string, :pointer, :ulong, :pointer, :pointer, :pointer, :pointer, :ulong], :bool
+    [:string, :pointer, :dword, :pointer, :pointer, :pointer, :pointer, :dword], :bool
 
   attach_pfunc :CreateProcessW,
     [:buffer_in, :buffer_in, :pointer, :pointer, :bool,
-     :ulong, :buffer_in, :buffer_in, :pointer, :pointer], :bool
+     :dword, :buffer_in, :buffer_in, :pointer, :pointer], :bool
 
-  attach_pfunc :AssignProcessToJobObject, [:ulong, :ulong], :bool
-  attach_pfunc :CreateJobObjectA, [:pointer, :string], :ulong
-  attach_pfunc :OpenJobObjectA, [:ulong, :bool, :string], :ulong
-  attach_pfunc :QueryInformationJobObject, [:ulong, :int, :pointer, :ulong, :pointer], :bool
-  attach_pfunc :SetInformationJobObject, [:ulong, :int, :pointer, :ulong], :bool
+  attach_pfunc :AssignProcessToJobObject, [:handle, :handle], :bool
+  attach_pfunc :CreateJobObjectA, [:pointer, :string], :handle
+  attach_pfunc :OpenJobObjectA, [:dword, :bool, :string], :handle
+  attach_pfunc :QueryInformationJobObject, [:handle, :int, :pointer, :dword, :pointer], :bool
+  attach_pfunc :SetInformationJobObject, [:handle, :int, :pointer, :dword], :bool
 
   ffi_lib :advapi32
 
   attach_pfunc :ConvertSidToStringSidA, [:buffer_in, :pointer], :bool
-  attach_pfunc :GetTokenInformation, [:ulong, :int, :pointer, :ulong, :pointer], :bool
-  attach_pfunc :OpenProcessToken, [:ulong, :ulong, :pointer], :bool
+  attach_pfunc :GetTokenInformation, [:handle, :int, :pointer, :dword, :pointer], :bool
+  attach_pfunc :OpenProcessToken, [:handle, :dword, :pointer], :bool
 
   attach_pfunc :CreateProcessWithLogonW,
-    [:buffer_in, :buffer_in, :buffer_in, :ulong, :buffer_in, :buffer_in,
-     :ulong, :buffer_in, :buffer_in, :pointer, :pointer], :bool
+    [:buffer_in, :buffer_in, :buffer_in, :dword, :buffer_in, :buffer_in,
+     :dword, :buffer_in, :buffer_in, :pointer, :pointer], :bool
 
   ffi_lib FFI::Library::LIBC
 
-  attach_pfunc :get_osfhandle, :_get_osfhandle, [:int], :ulong
+  attach_pfunc :get_osfhandle, :_get_osfhandle, [:int], :intptr_t
 
   begin
     attach_pfunc :get_errno, :_get_errno, [:pointer], :int
