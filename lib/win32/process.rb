93e9a837170f6cf67b5e8af284914581deb152a3
Daniel Berger <djberg96@gmail.com>
2013-10-21 12:58:38 -0600

    Version bumps.

--- lib/win32/process.rb
+++ lib/win32/process.rb
@@ -11,7 +11,7 @@ module Process
   extend Process::Constants
 
   # The version of the win32-process library.
-  WIN32_PROCESS_VERSION = '0.7.3'
+  WIN32_PROCESS_VERSION = '0.7.4'
 
   # Disable popups. This mostly affects the Process.kill method.
   SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX)
