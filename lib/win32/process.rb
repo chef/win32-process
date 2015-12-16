6b380f450aebb69d44bb7accd958ecb6b9e1d246
Daniel Berger <djberg96@gmail.com>
2015-12-16 15:43:48 -0700

    Version bump.

--- lib/win32/process.rb
+++ lib/win32/process.rb
@@ -10,7 +10,7 @@ module Process
   extend Process::Constants
 
   # The version of the win32-process library.
-  WIN32_PROCESS_VERSION = '0.8.2'
+  WIN32_PROCESS_VERSION = '0.8.3'
 
   # Disable popups. This mostly affects the Process.kill method.
   SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX)
--- test/test_win32_process.rb
+++ test/test_win32_process.rb
@@ -25,7 +25,7 @@ class TC_Win32Process < Test::Unit::TestCase
   end
 
   test "win32-process version is set to the correct value" do
-    assert_equal('0.8.2', Process::WIN32_PROCESS_VERSION)
+    assert_equal('0.8.3', Process::WIN32_PROCESS_VERSION)
   end
 
   test "create basic functionality" do
