871f545c9bf9844341db288c1ba604be775bfa5b
Jeremiah Snapp <jeremiah@chef.io>
2022-04-13 08:21:07 -0400

    Test
    Signed-off-by: Jeremiah Snapp <jeremiah@chef.io>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -3,6 +3,16 @@ $ErrorActionPreference = "Stop"
 
 # This will run ruby test on windows platform
 
+Write-Output 'Downloading Ruby + DevKit'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object System.Net.WebClient).DownloadFile('https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.1.1-1/rubyinstaller-devkit-3.1.1-1-x64.exe', 'c:\\rubyinstaller-devkit-3.1.1-1-x64.exe')
+
+Write-Output 'Installing Ruby + DevKit'
+Start-Process c:\rubyinstaller-devkit-3.1.1-1-x64.exe -ArgumentList '/verysilent /dir=C:\\ruby31' -Wait 
+
+Write-Output 'Cleaning up installation'
+Remove-Item c:\rubyinstaller-devkit-3.1.1-1-x64.exe -Force
+
 Write-Output "--- Bundle install"
 
 bundle config --local path vendor/bundle
